a26e63d00d302fc64fd34ae62b4f1ab2d18395ed
UllasUKrishnan <krishnanullas3@gmail.com>
2021-12-21 16:28:16 +0530

    navbar footer sidebar created

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -7,7 +7,32 @@ import { useCartContext } from '../context/cart_context'
 import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
-  return <h4>cart buttons </h4>
+  return <> <Wrapper className='cart-btn-wrapper'>
+    <Link to='/cart' className='cart-btn'/* onClick={closeSidebar}*/ >
+      Cart
+      <span className='cart-container'>
+        <FaShoppingCart />
+        <span className='cart-value'>{/* total_items*/} 2</span>
+      </span>
+    </Link>
+    {true ? (
+      <button
+        type='button'
+        className='auth-btn'
+        onClick={() => {
+          // clearCart()
+          localStorage.removeItem('user')
+          // logout({ returnTo: window.location.origin })
+        }}
+      >
+        Logout <FaUserMinus />
+      </button>
+    ) : (
+      <button type='button' className='auth-btn' /* onClick={loginWithRedirect}*/ >
+        Login <FaUserPlus />
+      </button>
+    )}
+  </Wrapper> </>
 }
 
 const Wrapper = styled.div`
