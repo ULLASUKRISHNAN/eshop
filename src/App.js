d89ea25457a818c3e8ff529ae31959a009eec623
UllasUKrishnan <krishnanullas3@gmail.com>
2021-12-21 17:20:02 +0530

    home page hero createad

--- src/App.js
+++ src/App.js
@@ -37,9 +37,9 @@ function App() {
             <Products />
           </Route>
           <Route exact path='/products/:id' children={<SingleProduct />} />
-          <PrivateRoute exact path='/checkout'>
+          <Route exact path='/checkout'>
             <Checkout />
-          </PrivateRoute>
+          </Route>
           <Route path='*'>
             <Error />
           </Route>
