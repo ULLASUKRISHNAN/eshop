04ea242812808a3b299c053e1e126c7bee6d6bc0
UllasUKrishnan <krishnanullas3@gmail.com>
2021-12-21 18:59:56 +0530

    implemented featured products

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -7,7 +7,22 @@ import Loading from './Loading'
 import Product from './Product'
 
 const FeaturedProducts = () => {
-  return <h4>featured products</h4>
+  const { featured_products } = useProductsContext()
+  return <>
+    <Wrapper className='section'>
+      <div className='title'>
+        <h2>featured products</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='section-center featured'>
+        {featured_products.slice(0, 3).map((product) => {
+          return <Product key={product.id} {...product} />
+        })}
+      </div>
+      <Link to='/products' className='btn'>
+        all products
+      </Link>
+    </Wrapper></>
 }
 
 const Wrapper = styled.section`
