cdf301c9284a5d302a8975a6dd393a2e745964ef
UllasUKrishnan <krishnanullas3@gmail.com>
2021-12-23 19:56:46 +0530

    created the private route for the checkout

--- src/App.js
+++ src/App.js
@@ -19,34 +19,34 @@ function App() {
   return (
     <>
 
-      {/* <AuthWrapper> */}
-      <Router>
-        <Navbar />
-        <Sidebar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/about'>
-            <About />
-          </Route>
-          <Route exact path='/cart'>
-            <Cart />
-          </Route>
-          <Route exact path='/products'>
-            <Products />
-          </Route>
-          <Route exact path='/products/:id' children={<SingleProduct />} />
-          <PrivateRoute exact path='/checkout'>
-            <Checkout />
-          </PrivateRoute>
-          <Route path='*'>
-            <Error />
-          </Route>
-        </Switch>
-        <Footer />
-      </Router>
-      {/* </AuthWrapper> */}
+      <AuthWrapper>
+        <Router>
+          <Navbar />
+          <Sidebar />
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/about'>
+              <About />
+            </Route>
+            <Route exact path='/cart'>
+              <Cart />
+            </Route>
+            <Route exact path='/products'>
+              <Products />
+            </Route>
+            <Route exact path='/products/:id' children={<SingleProduct />} />
+            <PrivateRoute exact path='/checkout'>
+              <Checkout />
+            </PrivateRoute>
+            <Route path='*'>
+              <Error />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </AuthWrapper>
 
     </>
   )
